2af3342670793b861790d48e3173e3933a47e23f
Wei Zhang <wzam@amazon.com>
2023-07-04 23:46:39 +0000

    update document for efa_rdm_ope
    Signed-off-by: Wei Zhang <wzam@amazon.com>

--- search/enums_65.js
+++ search/enums_65.js
@@ -3,8 +3,8 @@ var searchData=
   ['efa_5ferrno',['efa_errno',['../efa__errno_8h.html#a7690ea83fd80c37250d96c77a53d88fe',1,'efa_errno.h']]],
   ['efa_5ffork_5fsupport_5fstatus',['efa_fork_support_status',['../efa__fork__support_8h.html#ab95a6204a0cb4a62ec95aa80b415d1c2',1,'efa_fork_support.h']]],
   ['efa_5frdm_5fcuda_5fcopy_5fmethod',['efa_rdm_cuda_copy_method',['../efa__rdm__ope_8h.html#acfa7c40e76455a0248fb496a26d9583b',1,'efa_rdm_ope.h']]],
-  ['efa_5frdm_5fope_5fstate',['efa_rdm_ope_state',['../efa__rdm__ope_8h.html#a6c9901821c399c33346c2a539e688a30',1,'efa_rdm_ope.h']]],
-  ['efa_5frdm_5fope_5ftype',['efa_rdm_ope_type',['../efa__rdm__ope_8h.html#a3560fc43be30d9394c75582ad73c1e4e',1,'efa_rdm_ope.h']]],
+  ['efa_5frdm_5fope_5fstate',['efa_rdm_ope_state',['../structefa__rdm__ope.html#a6c9901821c399c33346c2a539e688a30',1,'efa_rdm_ope']]],
+  ['efa_5frdm_5fope_5ftype',['efa_rdm_ope_type',['../structefa__rdm__ope.html#a3560fc43be30d9394c75582ad73c1e4e',1,'efa_rdm_ope']]],
   ['efa_5frdm_5fpke_5falloc_5ftype',['efa_rdm_pke_alloc_type',['../efa__rdm__pke_8h.html#a9457b75b4705ca844507d177c3259cbe',1,'efa_rdm_pke.h']]],
   ['efa_5frdm_5frma_5fcontext_5fpkt_5ftype',['efa_rdm_rma_context_pkt_type',['../efa__rdm__pke__nonreq_8h.html#a39785b4ed20f9ef7c0e6907220ac91e3',1,'efa_rdm_pke_nonreq.h']]]
 ];
